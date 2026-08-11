09fdfbabaa3cfa4a380a73ef2197187620673519
NDH <ndhphysics@gmail.com>
2026-08-11 18:20:25 +0900

    add: BodyData에 대한 파서와 row 정의

---
+++ Source/Chimera/Data/Body/CMBodyTableRow.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "CMBodyTableRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct CHIMERA_API FCMBodyTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName RowName = NAME_None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString ID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString BodyType;
+};
---
+++ Source/ChimeraEditor/Parser/BodyDataParser.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BodyDataParser.h"
+#include "Data/Body/CMBodyTableRow.h"
+#include "Parser/SheetDataTableUtils.h"
+#include "Parser/SheetParserUtils.h"
+#include "Parser/SheetValidation.h"
+
+namespace BodyColumns
+{
+    const FString RowName = TEXT("RowName");
+    const FString ID = TEXT("ID");
+    const FString BodyType = TEXT("BodyType");
+
+    const TArray<FString> RequiredHeaders =
+    {
+        ID,
+        BodyType,
+    };
+}
+
+bool UBodyDataParser::OnParseComplete(FString& OutError)
+{
+    using namespace SheetDataTableUtils;
+    using namespace SheetParserUtils;
+    using namespace SheetValidation;
+
+    static constexpr const TCHAR* ParserName = TEXT("BodyData");
+    FParseReport Report;
+    OutError.Reset();
+
+    if (!ValidateTargetTable(
+            ParserName,
+            TargetTable,
+            FCMBodyTableRow::StaticStruct(),
+            OutError))
+    {
+        return false;
+    }
+
+    ValidateRequiredHeaders(GetHeaders(), BodyColumns::RequiredHeaders, &Report);
+    if (Report.HasErrors())
+    {
+        return FinalizeParseReport(ParserName, Report, OutError);
+    }
+
+    FScopedDataTableEditNotification TableEdit(TargetTable);
+
+    for (int32 Index = 0; Index < GetRowCount(); ++Index)
+    {
+        TMap<FString, FString> RowData;
+        if (!GetRowAt(Index, RowData))
+        {
+            continue;
+        }
+
+        FSheetRowReader Row(RowData, Index, Report);
+        FCMBodyTableRow NewRow;
+        
+        NewRow.RowName = Row.GetRequiredName(BodyColumns::RowName);
+        NewRow.ID = Row.Get(BodyColumns::ID);
+        NewRow.BodyType = Row.Get(BodyColumns::BodyType);
+
+        if (!Row.IsValid())
+        {
+            continue;
+        }
+
+        TargetTable->AddRow(NewRow.RowName, NewRow);
+        Report.AddSuccess();
+    }
+
+    return FinalizeParseReport(ParserName, Report, OutError);
+}
