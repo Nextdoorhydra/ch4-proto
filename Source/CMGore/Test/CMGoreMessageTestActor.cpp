b678cfaf5ce0d7eecc2a2d9c17280c79c1ae6610
NDH <ndhphysics@gmail.com>
2026-08-19 09:15:23 +0900

    Add: CMBloodEvent 메시지 테스트 파일

---
+++ Source/CMGore/Test/CMGoreMessageTestActor.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CMGoreMessageTestActor.h"
+#include "CMGoreGameplayTags.h"
+#include "CMGoreMessages.h"
+#include "GameplayMessageRuntime/Public/GameFramework/GameplayMessageSubsystem.h"
+
+
+
+// Sets default values
+ACMGoreMessageTestActor::ACMGoreMessageTestActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACMGoreMessageTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FCMBloodImpactMessage Message;
+
+	Message.Source = this;
+
+	Message.Location = GetActorLocation();
+
+	Message.SurfaceNormal = FVector::UpVector;
+
+	Message.Direction = FVector::ForwardVector;
+
+	Message.Intensity = 2.5f;
+
+	Message.BloodDefinitionId = TEXT("TestBlood");
+
+	UGameplayMessageSubsystem::Get(this).BroadcastMessage(
+		CMGoreGameplayTags::Message::Blood::Impact,
+		Message
+	);
+}
+
+// Called every frame
+void ACMGoreMessageTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/CMGore/Test/CMGoreMessageTestActor.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CMGoreMessageTestActor.generated.h"
+
+UCLASS()
+class CMGORE_API ACMGoreMessageTestActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACMGoreMessageTestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
