59deb47d3d80c191fcc68366020b2b9af9d474a5
RheaGums <dhwlsals99@gmail.com>
2026-09-10 21:00:14 +0900

    feat: 문 움직일 때 소리 추가 + 클리어 문 추가 == 소리다름 + 카메라 쉐이크
    feat: 문 움직일 때 소리 추가 + 클리어 문 추가 == 소리다름 + 카메라 쉐이크

--- Source/CMSound/Sound/CMSoundTags.cpp
+++ Source/CMSound/Sound/CMSoundTags.cpp
@@ -41,6 +41,8 @@ namespace CMSoundTags
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_Lever_Settle, "Chimera.Sound.Stage.Lever.Settle", "레버 끝 위치 정착");
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_VisionStone_OnLoop, "Chimera.Sound.Stage.VisionStone.OnLoop", "시야석 조건 충족 루프");
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_VisionStone_OffLoop, "Chimera.Sound.Stage.VisionStone.OffLoop", "시야석 조건 미충족 루프");
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_Door_Move, "Chimera.Sound.Stage.Door.Move", "일반 문 이동");
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_Door_StageClearMove, "Chimera.Sound.Stage.Door.StageClearMove", "스테이지 클리어 문 이동");
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_Door_Open, "Chimera.Sound.Stage.Door.Open", "스테이지 문 열림");
     UE_DEFINE_GAMEPLAY_TAG_COMMENT(Stage_Door_Close, "Chimera.Sound.Stage.Door.Close", "스테이지 문 닫힘");
 
--- Source/Chimera/Stage/Device/CMStageDoorBase.cpp
+++ Source/Chimera/Stage/Device/CMStageDoorBase.cpp
@@ -1,8 +1,14 @@
 #include "Stage/Device/CMStageDoorBase.h"
 
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/CMSoundPlayback.h"
+#include "Sound/CMSoundTags.h"
+
 ACMStageDoorBase::ACMStageDoorBase()
 {
     bStartActive = false;
+    MovementSoundTag = CMSoundTags::Stage_Door_Move;
 }
 
 // 문 표현과 블로킹 충돌이 목표 상태에 도달했음을 대기 중인 룸 트리거에 전달
@@ -11,10 +17,39 @@ void ACMStageDoorBase::NotifyDoorTransitionFinished(bool bIsOpen)
     OnDoorTransitionFinished.Broadcast(bIsOpen);
 }
 
+void ACMStageDoorBase::TestToggleDoor()
+{
+    if (const UWorld* World = GetWorld();
+        World && World->IsGameWorld() && HasAuthority())
+    {
+        ToggleElement();
+    }
+}
+
 // Mechanism 활성 상태를 문의 열림 상태로 전달
 void ACMStageDoorBase::HandleElementActiveChanged_Implementation(bool bIsActive)
 {
     Super::HandleElementActiveChanged_Implementation(bIsActive);
+
+    const bool bStateChanged = bHasObservedOpenState
+        && bLastObservedOpenState != bIsActive;
+    bLastObservedOpenState = bIsActive;
+    bHasObservedOpenState = true;
+
+    if (bStateChanged)
+    {
+        FCMSoundPlayback::PlaySFXAtActor(this, MovementSoundTag);
+        if (bIsActive && OpeningCameraShake)
+        {
+            UGameplayStatics::PlayWorldCameraShake(
+                this,
+                OpeningCameraShake,
+                GetActorLocation(),
+                CameraShakeInnerRadius,
+                FMath::Max(CameraShakeOuterRadius, CameraShakeInnerRadius));
+        }
+    }
+
     OnDoorOpenStateChanged(bIsActive);
 }
 
